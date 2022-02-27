5f4bbf01c9dbe75ca2b1439455d92641e355a498
Alejandro <alejomedina161011@gmail.com>
2022-02-27 12:10:00 -0400

    segundo commit

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,14 +4,14 @@ import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(private readonly authService: AuthService) {}
 
   @Post('signup')
   signup(@Body() dto: AuthDto) {
     return this.authService.signup(dto);
   }
   @Post('signin')
-  signin(): string {
-    return 'im in signin';
+  signin(@Body() dto: AuthDto): any {
+    return this.authService.signin(dto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,8 +7,12 @@ import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 @Injectable({})
 export class AuthService {
   constructor(private readonly prisma: PrismaService) {}
+  //****metodo signup****
   async signup(dto: AuthDto) {
+    //creamos el hash de la contraseña
     const hash = await argon.hash(dto.password);
+    //guardamos el usuario en la base de datos
+    // usamos el try catch para capturar el error
     try {
       const user = await this.prisma.user.create({
         data: {
@@ -16,6 +20,8 @@ export class AuthService {
           hash,
         },
       });
+      //despues de guardar al usuario borramos el hash para que no se muestre en la respuesta
+      delete user.hash;
       return user;
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
@@ -26,8 +32,26 @@ export class AuthService {
       throw error;
     }
   }
-
-  signin() {
-    return { msg: 'I have signed in' };
+  //****metodo signin****
+  async signin(dto: AuthDto) {
+    //buscamos el usuario por el email
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    //si no existe devolvemos un error
+    if (!user) {
+      throw new ForbiddenException('User not found');
+    }
+    //Si el usuario existe comparamos la contraseña con el hash
+    const pwMatch = await argon.verify(user.hash, dto.password);
+    //si no coincide devolvemos un error
+    if (!pwMatch) {
+      throw new ForbiddenException('Invalid credentials');
+    }
+    //si todo es correcto borramos el hash y mostramos el usuario
+    delete user.hash;
+    return user;
   }
 }
